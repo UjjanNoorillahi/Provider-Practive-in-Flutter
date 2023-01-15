eec4e6bf8a7ba48c4b91cfa7d8e8b5a514ff30ea
UjjanNoorillahi <69105391+UjjanNoorillahi@users.noreply.github.com>
2023-01-15 09:55:23 +0500

    2nd Example of Stateless Widget as Statefull widget with visibablity and visability_false icon to show and hide the password

--- lib/screens/favourite_screen/value_notfiy_listener.dart
+++ lib/screens/favourite_screen/value_notfiy_listener.dart
@@ -6,6 +6,7 @@ class NotifyListenerScreen extends StatelessWidget {
 // here we don't need to create the provider class in the provider directory to
 //set the value of the counter following ValueNotifier line done all the things according to the
   ValueNotifier<int> _counter = ValueNotifier<int>(0);
+  ValueNotifier<bool> toggle = ValueNotifier<bool>(true);
 
   @override
   Widget build(BuildContext context) {
@@ -15,15 +16,37 @@ class NotifyListenerScreen extends StatelessWidget {
         title: const Text("Statless Widget as a Statefull Widget"),
       ),
       // ignore: prefer_const_constructors
-      body: Center(
-          child: ValueListenableBuilder(
-              valueListenable: _counter,
+      body: Column(
+        children: [
+          ValueListenableBuilder(
+              valueListenable: toggle,
               builder: ((context, value, child) {
-                return Text(
-                  value.toString(),
-                  style: const TextStyle(fontSize: 50),
+                return TextFormField(
+                  obscureText: toggle.value,
+                  // ignore: prefer_const_constructors
+                  decoration: InputDecoration(
+                      hintText: 'Password',
+                      suffix: InkWell(
+                        onTap: () {
+                          toggle.value = !toggle.value;
+                        },
+                        child: Icon(toggle.value
+                            ? Icons.visibility_off
+                            : Icons.visibility),
+                      )),
                 );
-              }))),
+              })),
+          Center(
+              child: ValueListenableBuilder(
+                  valueListenable: _counter,
+                  builder: ((context, value, child) {
+                    return Text(
+                      value.toString(),
+                      style: const TextStyle(fontSize: 50),
+                    );
+                  })))
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _counter.value++;
